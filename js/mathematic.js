d332f80964239ff2430787641b85a8cfc7350f68
muondu <munenemuondu@gmail.com>
2019-08-31 12:04:54 +0300

    I am duing a project about a food shop

---
+++ js/mathematic.js
@@ -0,0 +1,15 @@
+//The array of a rectangle
+function areaOfARectangle(num1,num2){
+    return num1 * num2;
+}
+
+
+//The perimeter of a rectangle
+function perimeterOfARectangle(length,width){
+    return (length + width) * 2;
+}
+
+
+let theArea = areaOfARectangle(2,4);
+let thePerimeter = perimeterOfARectangle(1, 2);
+
